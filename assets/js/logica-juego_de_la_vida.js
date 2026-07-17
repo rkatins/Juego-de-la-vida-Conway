bb7d4b69bba6901ebe923cfb9d6e779289d470c1
Rubén Katins Durán <katinsruben.98@gmail.com>
2026-07-17 23:39:31 +0200

    Correccion sintaxis

--- assets/js/logica-juego_de_la_vida.js
+++ assets/js/logica-juego_de_la_vida.js
@@ -13,7 +13,7 @@ class JuegoDeLaVida {
     /**
      * Reinicia el tablero, poniendo todas las células como muertas (0)
      */
-    reiniciar() {
+    fReiniciarJuego() {
         this.grid = [];
         for (let i = 0; i < this.tamagno; i++) {
             this.grid.push(new Array(this.tamagno).fill(0));
